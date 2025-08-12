cc590dd4ab1ddc76a1df8fb486651264d9250c9c
Alex Balak <alex.balak@outlook.com>
2025-08-12 13:54:05 +0200

    empty quill

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -47,56 +47,12 @@ interface TextChangeData {
 }
 
 const App: React.FC = () => {
-  const [range, setRange] = useState<RangeType | null>(null);
-  const [lastChange, setLastChange] = useState<TextChangeData | null>(null);
-  const [readOnly, setReadOnly] = useState<boolean>(false);
-
   // Use a ref to access the quill instance directly
   const quillRef = useRef<Quill | null>(null);
 
   return (
     <div>
-      <Editor
-        ref={quillRef}
-        readOnly={readOnly}
-        defaultValue={new Delta()
-          .insert('Hello')
-          .insert('\n', { header: 1 })
-          .insert('Some ')
-          .insert('initial', { bold: true })
-          .insert(' ')
-          .insert('content', { underline: true })
-          .insert('\n')}
-        onSelectionChange={setRange}
-        onTextChange={setLastChange}
-      />
-      <div className="controls">
-        <label>
-          Read Only:{' '}
-          <input
-            type="checkbox"
-            checked={readOnly}
-            onChange={(e) => setReadOnly(e.target.checked)}
-          />
-        </label>
-        <button
-          className="controls-right"
-          type="button"
-          onClick={() => {
-            alert(quillRef.current?.getLength());
-          }}
-        >
-          Get Content Length
-        </button>
-      </div>
-      <div className="state">
-        <div className="state-title">Current Range:</div>
-        {range ? JSON.stringify(range) : 'Empty'}
-      </div>
-      <div className="state">
-        <div className="state-title">Last Change:</div>
-        {lastChange ? JSON.stringify(lastChange.delta.ops) : 'Empty'}
-      </div>
+      <Editor ref={quillRef} />
     </div>
   );
 };
