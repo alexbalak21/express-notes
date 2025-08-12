fa2e435fa4f0edb4224fce7ccbc28d9c46ae979f
Alex Balak <alex.balak@outlook.com>
2025-08-12 15:13:50 +0200

    update

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,36 +1,9 @@
 import * as React from 'react';
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 import Quill from 'quill';
 import 'quill/dist/quill.snow.css';
 import Editor from './Editor';
 
-interface DeltaOperation {
-  insert?: string | object;
-  delete?: number;
-  retain?: number;
-  attributes?: Record<string, any>;
-}
-
-interface DeltaStatic {
-  ops?: DeltaOperation[];
-  insert(content: string, attributes?: Record<string, any>): DeltaStatic;
-  delete(length: number): DeltaStatic;
-  retain(length: number, attributes?: Record<string, any>): DeltaStatic;
-  concat(other: DeltaStatic): DeltaStatic;
-  filter(predicate: (op: DeltaOperation) => boolean): DeltaOperation[];
-  forEach(predicate: (op: DeltaOperation) => void): void;
-  length(): number;
-  map<T>(predicate: (op: DeltaOperation) => T): T[];
-  partition(
-    predicate: (op: DeltaOperation) => boolean
-  ): [DeltaOperation[], DeltaOperation[]];
-  reduce<T>(
-    predicate: (acc: T, curr: DeltaOperation, idx: number, arr: DeltaOperation[]) => T,
-    initial: T
-  ): T;
-  slice(start?: number, end?: number): DeltaStatic;
-  chop(): DeltaStatic;
-}
 
 
 
